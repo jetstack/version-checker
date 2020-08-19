69ec5aa79c1705d84060fb758533b9a2c6977120
JoshVanL <vleeuwenjoshua@gmail.com>
2020-08-19 17:54:22 +0100

    Adds better docker parsing with tests
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

---
+++ pkg/client/docker/path.go
@@ -0,0 +1,25 @@
+package docker
+
+import (
+	"regexp"
+	"strings"
+)
+
+var (
+	reg = regexp.MustCompile(`(^(.*\.)?docker.com$)|(^(.*\.)?docker.io$)`)
+)
+
+func (c *Client) IsHost(host string) bool {
+	return reg.MatchString(host)
+}
+
+func (c *Client) RepoImageFromPath(path string) (string, string) {
+	split := strings.Split(path, "/")
+
+	lenSplit := len(split)
+	if lenSplit == 1 {
+		return "library", split[0]
+	}
+
+	return split[lenSplit-2], split[lenSplit-1]
+}
