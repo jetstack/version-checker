a27adec876bdace1e1269d67a41df6b336713d78
JoshVanL <vleeuwenjoshua@gmail.com>
2020-08-22 17:04:17 +0100

    Move controller search into sub controller module using generic cache
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

---
+++ pkg/controller/search/search.go
@@ -0,0 +1,94 @@
+package search
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/jetstack/version-checker/pkg/api"
+	"github.com/jetstack/version-checker/pkg/cache"
+	"github.com/jetstack/version-checker/pkg/version"
+)
+
+// Search is the implementation for the searching and caching of image URLs.
+type Search struct {
+	log *logrus.Entry
+
+	versionGetter *version.Version
+	searchCache   *cache.Cache
+}
+
+// searchCacheItem is a single node item for the cache of a lastest image
+// search. This cache is periodically garbage collected.
+type searchCacheItem struct {
+	itemMu      sync.Mutex
+	timestamp   time.Time
+	latestImage *api.ImageTag
+}
+
+// New creates a new Search for querying searches over image tags
+func New(log *logrus.Entry, cacheTimeout time.Duration, versionGetter *version.Version) *Search {
+	s := &Search{
+		log:           log.WithField("module", "search"),
+		versionGetter: versionGetter,
+	}
+
+	s.searchCache = cache.New(s.log, cacheTimeout, s)
+
+	return s
+}
+
+func (s *Search) Fetch(ctx context.Context, imageURL string, optsI interface{}) (interface{}, error) {
+	opts := optsI.(*api.Options)
+
+	latestImage, err := s.versionGetter.LatestTagFromImage(ctx, imageURL, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return latestImage, err
+}
+
+// LatestImage will get the latestImage image given an image URL and
+// options. If not found in the cache, or is too old, then will do a fresh
+// lookup and commit to the cache.
+func (s *Search) LatestImage(ctx context.Context, log *logrus.Entry,
+	imageURL string, opts *api.Options) (*api.ImageTag, error) {
+	hashIndex, err := calculateHashIndex(imageURL, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	lastestImage, err := s.searchCache.Get(ctx, hashIndex, imageURL, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return lastestImage.(*api.ImageTag), nil
+}
+
+// Run will run the search and image cache garbage collectors.
+func (s *Search) Run(refreshRate time.Duration) {
+	go s.versionGetter.Run(refreshRate)
+	s.searchCache.StartGarbageCollector(refreshRate)
+}
+
+// calculateHashIndex returns a hash index given an imageURL and options.
+func calculateHashIndex(imageURL string, opts *api.Options) (string, error) {
+	opsJson, err := json.Marshal(opts)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal options: %s", err)
+	}
+
+	hash := fnv.New32()
+	if _, err := hash.Write(append(opsJson, []byte(imageURL)...)); err != nil {
+		return "", fmt.Errorf("failed to calculate search hash: %s", err)
+	}
+
+	return fmt.Sprintf("%d", hash.Sum32()), nil
+}
